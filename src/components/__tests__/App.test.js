f91c63e3f6ea4045d1882c541b6736e68123a2e4
Rachel Rondon <rrondon@medtouch.com>
2020-03-02 16:47:18 -0500

    add enzyme for tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,5 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "../App";
+import CommentBox from "../CommentBox";
 
-it('shows a comment box')
+it('shows a comment box', () => {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentBox))
+});
